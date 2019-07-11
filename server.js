3e47d9d07e27b0d113213814e66231a1964b1339
Matt Winzer <matt.winzer@gmail.com>
2019-07-11 13:38:11 -0600

    stripe working

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const PORT = process.env.PORT || 4000
 const app = express()
@@ -68,7 +70,30 @@ app.get('/api/products/:id', (req, res, next) => {
     .catch(error => {
       console.log(error)
     })
+})
 
+app.post('/api/checkout', async (req, res, next) => {
+  const lineItems = [{
+    name: 'T-shirt',
+    description: 'Comfortable cotton t-shirt',
+    images: ['http://lorempixel.com/400/200/'],
+    amount: 500,
+    currency: 'usd',
+    quantity: 1,
+  }]
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: lineItems,
+      success_url: 'http://localhost:3000/success',
+      cancel_url: 'http://localhost:3000/cancel',
+    })
+    res.json({ session })
+  }
+  catch (error) {
+    res.status(400).json({ error })
+  }
 })
  
 // Error handling
@@ -92,3 +117,60 @@ app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`)
 })
 
+
+
+
+
+// const createCheckoutSession = async (req, res) => {
+//   const lineItems = createLineItems(req.body)
+
+//   try {
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ['card'],
+//       line_items: lineItems,
+//       success_url: 'http://localhost:3000/purchase/success',
+//       cancel_url: 'http://localhost:3000/purchase/cancel',
+//     })
+//     res.json({ session })
+//   }
+//   catch (error) {
+//     res.status(400).json({ error })
+//   }
+// }
+
+// const fulfillCheckoutPurchase = (req, res) => {
+//   console.log('fulfilling')
+//   const sig = req.headers['stripe-signature']
+//   let event
+
+//   try {
+//     event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret)
+//   } catch (err) {
+//     return res.status(400).send(`Webhook Error: ${err.message}`)
+//   }
+
+//   // Handle the checkout.session.completed event
+//   if (event.type === 'checkout.session.completed') {
+//     const session = event.data.object
+
+//     // Fulfill the purchase...
+//     console.log('checkout session data', session)
+//   }
+
+//   // Return a response to acknowledge receipt of the event (to stripe)
+//   res.json({ received: true })
+// }
+
+// ---------------------------------------- HELPER FUNCTIONS ----------------------------------------
+
+// function createLineItems(reports) {
+//   return reports.map(({ market, state, price }) => {
+//     return {
+//       name: `${market}, ${state}`,
+//       description: `PDF report for ${market}, ${state}.`,
+//       amount: price * 100,
+//       currency: 'usd',
+//       quantity: 1
+//     }
+//   })
+// }
